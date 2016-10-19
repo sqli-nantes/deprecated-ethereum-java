5a66eedfa187b32d0e83fbdcd5feaed745d4b8cd
nigui <guin56@gmail.com>
2016-10-19 10:57:13 +0200

    Refactor project with creation of submodules core and sample

---
+++ ethereum-java-sample/src/main/java/ethereumjavasample/Main.java
@@ -0,0 +1,12 @@
+package ethereumjavasample;
+
+/**
+ * Created by root on 19/10/16.
+ */
+public class Main {
+
+    public void main(String[] args){
+        System.out.println("it works");
+    }
+
+}
