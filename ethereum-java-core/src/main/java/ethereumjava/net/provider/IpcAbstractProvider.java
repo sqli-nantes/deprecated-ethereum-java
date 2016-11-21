45224037e5f52c3930ce1fe3e6a13a6d1bcb6da9
nigui <guin56@gmail.com>
2016-11-21 15:42:54 +0100

    remove new Thread in IPC Provider

--- ethereum-java-core/src/main/java/ethereumjava/net/provider/IpcAbstractProvider.java
+++ ethereum-java-core/src/main/java/ethereumjava/net/provider/IpcAbstractProvider.java
@@ -1,15 +1,16 @@
 package ethereumjava.net.provider;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import ethereumjava.exception.EthereumJavaException;
 import ethereumjava.net.Request;
 import ethereumjava.net.Response;
 import rx.Observable;
 import rx.Subscriber;
-
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.util.List;
+import rx.functions.Action1;
+import rx.functions.Func1;
 
 /**
  * Created by gunicolas on 31/08/16.
@@ -52,46 +53,72 @@ public abstract class IpcAbstractProvider extends AbstractProvider {
     }
 
     private void startListening() throws EthereumJavaException {
-        listeningThread = new Thread(new Runnable() {
+
+        Observable.interval(500, TimeUnit.MILLISECONDS) //Thread computation
+                .map(readFile())
+                .filter(removeNullLines())
+                .subscribe(manageResponse());
+
+    }
+
+    private Subscriber<String> manageResponse() {
+        return new Subscriber<String>() {
             @Override
-            public void run() {
-                try {
-                    listen = true;
-                    while (listen) {
-                        try {
-                            String line;
-                            while (in.ready() && (line = in.readLine()) != null) {
-                                Response response = gson.fromJson(line, Response.class);
+            public void onCompleted() {
 
-                                if (response.request != null) {
+            }
 
-                                    List<Subscriber> subscribers = response.request.getSubscribers();
-                                    if (response.isError()) {
-                                        for (Subscriber subscriber : subscribers) {
-                                            subscriber.onError(new EthereumJavaException(response.error.message));
-                                        }
-                                    } else {
-                                        for (Subscriber subscriber : subscribers) {
-                                            subscriber.onNext(response.result);
-                                            subscriber.onCompleted();
-                                        }
-                                    }
-                                    requestQueue.remove(response.id);
+            @Override
+            public void onError(Throwable e) {
+                //TODO on error on ipc file
+            }
 
-                                } // else just ignored
-                            }
-                        }catch(Exception e ){
-                            throw new EthereumJavaException(e);
+            @Override
+            public void onNext(String line) {
+                Response response = gson.fromJson(line, Response.class);
+
+                if (response.request != null) {
+
+                    List<Subscriber> subscribers = response.request.getSubscribers();
+                    if (response.isError()) {
+                        for (Subscriber subscriber : subscribers) {
+                            subscriber.onError(new EthereumJavaException(response.error.message));
                         }
-                        Thread.sleep(500);
+                    } else {
+                        for (Subscriber subscriber : subscribers) {
+                            subscriber.onNext(response.result);
+                            subscriber.onCompleted();
+                        }
                     }
+                    requestQueue.remove(response.id);
                 }
-                catch(Exception e){
+            }
+        };
+    }
+    private Func1<String, Boolean> removeNullLines() {
+        return new Func1<String, Boolean>() {
+            @Override
+            public Boolean call(String s) {
+                return s == null;
+            }
+        };
+    }
+    private Func1<Long,String> readFile(){
+        return new Func1<Long, String>() {
+            @Override
+            public String call(Long aLong) {
+                try {
+                    if( ! in.ready() ){
+                        return null;
+                    } else {
+                        String line = in.readLine();
+                        return line;
+                    }
+                } catch (IOException e) {
                     throw new EthereumJavaException(e);
                 }
             }
-        });
-        listeningThread.start();
+        };
     }
 
     @Override
