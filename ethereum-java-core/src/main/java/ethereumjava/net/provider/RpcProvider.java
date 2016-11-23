cd8cf9138394ca0e968267b6e5e94f1cec68bfa8
nigui <guin56@gmail.com>
2016-11-23 11:21:23 +0100

    Refactor RPCProvider + add asynchronism in sending request and receiving response onSubscribe

--- ethereum-java-core/src/main/java/ethereumjava/net/provider/RpcProvider.java
+++ ethereum-java-core/src/main/java/ethereumjava/net/provider/RpcProvider.java
@@ -13,7 +13,6 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.nio.charset.StandardCharsets;
 
 /**
  * Created by gunicolas on 18/08/16.
@@ -46,66 +45,89 @@ public class RpcProvider extends AbstractProvider {
 	protected Observable send(byte[] stringRequest, final Request request) {
 
 		try {
-			URLConnection urlConn = url.openConnection();
-			this.connection = (HttpURLConnection) urlConn;
-			this.connection.setAllowUserInteraction(false);
-			this.connection.setInstanceFollowRedirects(true);
-			this.connection.setRequestMethod("POST");
+			createConnection(stringRequest);
 
-			this.connection.setDoInput(true);
-			this.connection.setDoOutput(true);
-			this.connection.setChunkedStreamingMode(0);
-			this.connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-			this.connection.setRequestProperty("Accept", "application/json");
-			this.connection.connect();
+			return Observable.create(sendRequestAndNotify(request));
 
-			outputStream = this.connection.getOutputStream();
-			outputStream.write(stringRequest);
-			outputStream.flush();
-			inputStream = this.connection.getInputStream();
+		} catch (IOException e) {
+			closeConnection();
+			throw new EthereumJavaException(e);
+		}
 
+	}
+
+	private void createConnection(byte[] stringRequest) throws IOException {
+		URLConnection urlConn = url.openConnection();
+		this.connection = (HttpURLConnection) urlConn;
+		this.connection.setAllowUserInteraction(false);
+		this.connection.setInstanceFollowRedirects(true);
+		this.connection.setRequestMethod("POST");
+
+		this.connection.setDoInput(true);
+		this.connection.setDoOutput(true);
+		this.connection.setChunkedStreamingMode(0);
+		this.connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+		this.connection.setRequestProperty("Accept", "application/json");
+		this.connection.connect();
+
+		outputStream = this.connection.getOutputStream();
+		outputStream.write(stringRequest);
+		outputStream.flush();
+	}
+
+	private Observable.OnSubscribe<Object> sendRequestAndNotify(final Request request) {
+		return new Observable.OnSubscribe<Object>() {
+            @Override
+            public void call(Subscriber<? super Object> subscriber) {
+				String line = sendRequestAndGetResponse(request);
+				notifySubscriber(subscriber, line);
+            }
+        };
+	}
+
+	private void notifySubscriber(Subscriber<? super Object> subscriber, String line) {
+		Response response = gson.fromJson(line, Response.class);
+		if (response.request != null) {
+            if (response.isError()) {
+                subscriber.onError(new EthereumJavaException(response.error.message));
+            } else {
+                subscriber.onNext(response.result);
+                subscriber.onCompleted();
+            }
+        }
+		requestQueue.remove(response.id);
+	}
+	private String sendRequestAndGetResponse(Request request) {
+		try {
+			inputStream = connection.getInputStream();
 			in = new BufferedReader(new InputStreamReader(inputStream, CHARSET));
-
-			final String line;
-			if( in.ready() && (line = in.readLine()) != null ){
+			if (in.ready()) {
 				requestQueue.put(request.getId(),request);
-
-				Observable ret = Observable.create(new Observable.OnSubscribe<Object>() {
-					@Override
-					public void call(Subscriber<? super Object> subscriber) {
-						Response response = gson.fromJson(line, Response.class);
-						if (response.request != null) {
-							if (response.isError()) {
-								subscriber.onError(new EthereumJavaException(response.error.message));
-							} else {
-								subscriber.onNext(response.result);
-								subscriber.onCompleted();
-							}
-						}
-						requestQueue.remove(response.id);
-					}
-				});
-
-				return ret;
-
+				return in.readLine();
+			} else {
+				throw new EthereumJavaException("RPC BufferedReader not ready");
 			}
-
 		} catch (IOException e) {
 			throw new EthereumJavaException(e);
 		} finally {
-			this.connection.disconnect();
-			try {
-				if (outputStream != null) {
-					outputStream.close();
-				}
-				if (inputStream != null) {
-					inputStream.close();
-				}
-			} catch (IOException e) {
-				throw new EthereumJavaException(e);
-			}
+			closeConnection();
 		}
-		return null;
+	}
+
+	private void closeConnection() {
+		if( this.connection != null ) {
+			this.connection.disconnect();
+		}
+		try {
+            if (outputStream != null) {
+                outputStream.close();
+            }
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        } catch (IOException e) {
+            throw new EthereumJavaException(e);
+        }
 	}
 
 }
